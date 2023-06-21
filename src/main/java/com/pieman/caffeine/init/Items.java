cc8b1bed6e14503c691d09370b63dbf3acf9b9fe
Shaylin Simadari <ssi162@uclive.ac.nz>
2023-06-22 11:54:26 +1200

    fluids work all coffee products moved to food/ added nutrition values

--- src/main/java/com/pieman/caffeine/init/Items.java
+++ src/main/java/com/pieman/caffeine/init/Items.java
@@ -1,7 +1,6 @@
 package com.pieman.caffeine.init;
 
 import com.pieman.caffeine.fluids.FluidType;
-import com.pieman.caffeine.items.Coffee;
 import com.pieman.caffeine.items.Food;
 import net.dries007.tfc.common.TFCItemGroup;
 import net.dries007.tfc.common.items.DecayingItem;
@@ -17,7 +16,6 @@ import java.util.function.Supplier;
 public class Items {
     public static final DeferredRegister<Item> ITEMS;
     public static final Map<Food, RegistryObject<Item>> FOOD;
-    public static final Map<Coffee, RegistryObject<Item>> COFFEES;
     public static final Map<FluidType, RegistryObject<BucketItem>> FLUID_BUCKETS;
 
     private static <T extends Item> RegistryObject<T> register(String name, Supplier<T> item) {
@@ -29,22 +27,17 @@ public class Items {
 
         FOOD = Helpers.mapOfKeys(Food.class, (food) ->
             register("food/" + food.name(), () ->
-                //decayingitem with food properties (food props based on Food enum)
-                new DecayingItem(food.createProperties())
+                    food.decays ?
+                            new DecayingItem(food.createProperties()):
+                            new Item(food.createProperties())
             )
         );
 
-        COFFEES = Helpers.mapOfKeys(Coffee.class, (coffee) ->
-            register(coffee.name(), () ->
-                new Item(coffee.createProperties())
+        FLUID_BUCKETS = FluidType.mapOf((fluid) ->
+            register("bucket/" + fluid.name(), () ->
+                new BucketItem(fluid.fluid(), (new Item.Properties()).craftRemainder(net.minecraft.world.item.Items.BUCKET).stacksTo(1).tab(TFCItemGroup.MISC))
             )
         );
-
-        FLUID_BUCKETS = FluidType.mapOf((fluid) -> {
-            return register("bucket/" + fluid.name(), () -> {
-                return new BucketItem(fluid.fluid(), (new Item.Properties()).craftRemainder(net.minecraft.world.item.Items.BUCKET).stacksTo(1).tab(TFCItemGroup.MISC));
-            });
-        });
     }
 
 }
--- src/main/java/com/pieman/caffeine/items/Food.java
+++ src/main/java/com/pieman/caffeine/items/Food.java
@@ -1,21 +1,35 @@
 package com.pieman.caffeine.items;
 
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
 import net.dries007.tfc.common.TFCItemGroup;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 public enum Food {
-    COFFEE_CHERRIES(false, true);
+    COFFEE_CHERRIES(false, true, () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 100, 0), true),
+    COFFEE_CHERRIES_DRIED(false, false, () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 100, 0), true),
+    COFFEE_BEANS_GREEN(false, false, () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 0), true),
+    COFFEE_BEANS(false, false, () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 100, 0), false),
+    COFFEE_GROUND(false, false, () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 0), true);
+
     private final boolean meat;
     private final boolean fast;
+    private final List<Pair<Supplier<MobEffectInstance>, Float>> effects;
+    public final boolean decays;
 
-    Food() {
-        this(false, false);
-    }
+//    private final static MobEffectInstance SHORT_SPEED_EFFECT = new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 0);
 
-    Food(boolean meat, boolean fast) {
+    Food(boolean meat, boolean fast, Supplier<MobEffectInstance> effect, boolean decays) {
         this.meat = meat;
         this.fast = fast;
+        this.effects = Lists.newArrayList(new Pair<Supplier<MobEffectInstance>, Float>(effect, 1f));
+        this.decays = decays;
     }
 
     public FoodProperties getFoodProperties() {
@@ -26,6 +40,12 @@ public enum Food {
 
         if (this.fast) {
             builder.fast();
+        }
+
+        if(effects != null) {
+            for (Pair<Supplier<MobEffectInstance>, Float> effect : effects) {
+                builder.effect(effect.getFirst(), effect.getSecond());
+            }
         }
 
         return builder.nutrition(4).saturationMod(0.3F).build();
