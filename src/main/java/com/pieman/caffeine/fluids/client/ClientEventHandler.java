8ea3facf765792c0ed51d89e22424d8007a481d3
Shaylin Simadari <ssi162@uclive.ac.nz>
2023-06-24 14:09:53 +1200

    non functional drying mat + BlockEntity added

--- src/main/java/com/pieman/caffeine/fluids/client/ClientEventHandler.java
+++ src/main/java/com/pieman/caffeine/fluids/client/ClientEventHandler.java
@@ -1,10 +1,8 @@
-package com.pieman.caffeine.client;
+package com.pieman.caffeine.fluids.client;
 
-import com.pieman.caffeine.client.render.blockentity.DryingMatBlockEntityRenderer;
+import com.pieman.caffeine.fluids.client.render.blockentity.DryingMatBlockEntityRenderer;
 import com.pieman.caffeine.init.BlockEntities;
 import com.pieman.caffeine.init.Blocks;
-import net.dries007.tfc.client.render.blockentity.PlacedItemBlockEntityRenderer;
-import net.dries007.tfc.common.blockentities.TFCBlockEntities;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
@@ -13,7 +11,6 @@ import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ClientEventHandler {
 
